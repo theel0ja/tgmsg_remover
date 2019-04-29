a6e10b9119be4947e9253849dcceb089ff210719
Elias Ojala <git@eliasojala.me>
2019-04-29 14:07:25 +0300

    Group purge

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ targets = [
 ]  # tips for this: chat_id param in https://docs.pyrogram.ml/pyrogram/Client#pyrogram.Client.get_history
 
 
-your_username = "username"
+# "ANY" if group purge, your username if user purge
+your_username = "ANY"
+debug = True
 
 delete_before_time = 3 * 24
 # int() is to delete decimals
-delete_before = int(time.time() - (12 * 60 * 60))
+delete_before = int(time.time() - (delete_before_time * 60 * 60))
 
 #print(delete_before)
 
@@ -47,21 +49,26 @@ for target in targets:
 
         for message in messages:
             if(type(message.from_user.username) is str):
-                username = message.from_user.username
-                username = " from " + username
+                from_username = "ID {} (username: {})".format(message.from_user.id, message.from_user.username)
             else:
-                username = ""
+                from_username = "ID {} (no username)".format(message.from_user.id)
 
-            if message.from_user.username == your_username:
+            if your_username == "ANY" or message.from_user.username == your_username:
                 if(delete_before > message.date):
-                    print("Deleting message from " + your_username + " (sent " + str(message.date) + ")")
-                    app.delete_messages(message.chat.id, [message.message_id])
-                else:
-                    print("Read message from " + your_username + " (too new)")
+                    print("Deleting message from {} (sent {})"
+                        .format(from_username, str(message.date)))
 
-                #app.delete_messages(message.chat.id, [message.message_id])
+                    # Delete message
+                    if debug is True:
+                        print("Simulate deletion of message (debug mode is enabled)")
+                    else:
+                        app.delete_messages(message.chat.id, [message.message_id])
+                else:
+                    print("Read message (too new) from {}".format(from_username))
             else:
-                print("Read message" + username)
+                print("Read message from {}".format(from_username))
+
+            print("")
 
     app.stop()
 
